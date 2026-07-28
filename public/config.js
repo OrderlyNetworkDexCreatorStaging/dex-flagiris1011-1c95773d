9a8c929733204fb4932f737a5430f7e7227a7143
Jame Zhan <161272831+orderly-jame@users.noreply.github.com>
2026-07-28 23:00:26 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -21,7 +21,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Leaderboard,Points,Campaigns",
   "VITE_CUSTOM_MENUS": "",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
-  "VITE_ENABLE_CAMPAIGNS": "false",
+  "VITE_ENABLE_CAMPAIGNS": "true",
   "VITE_TELEGRAM_URL": "",
   "VITE_DISCORD_URL": "",
   "VITE_TWITTER_URL": "",
